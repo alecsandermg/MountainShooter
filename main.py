bc23b99f9eee5492349f5b6b7f827b851f7d86b7
Alecsander <alecsandermg@hotmail.com>
2025-08-04 19:44:00 -0300

    Creating main loop Connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import pygame as pg
+from code.game import Game
 
-pg.init()
-print('Setup Start')
-window =  pg.display.set_mode(size = (600, 480))
-print('Setup Finish')
 
-print('Loop start')
-while True:
-    #Check for all events
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit() #close window
-            print('Loop Finish')
-            quit() #end pygame
+game = Game()
+game.run()
