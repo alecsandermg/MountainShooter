671aab102ffe00a595102ed6ebcbe906cf1682cd
Alecsander <alecsandermg@hotmail.com>
2025-08-20 23:05:22 -0300

    Creating Players and Enemies Creating Players and Enemies movements

--- code/game.py
+++ code/game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame as pg
-
+from code.level import Level
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.menu import Menu
 from code.Const import WIN_WIDTH, WIN_HEIGHT
@@ -18,8 +18,7 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                from code.level import Level
-                level = Level(self.window, 'Level 1', menu_return)
+                level = Level(self.window, 'Level1', menu_return)
                 level_return = level.run()
             elif menu_return == MENU_OPTION[4]:
                 pg.quit()
--- code/level.py
+++ code/level.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+import random
+from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 import pygame as pg
+from pygame import Surface, Rect
+from pygame.font import Font
 
 class Level:
     def __init__(self, window, name, game_mode):
@@ -11,10 +16,38 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        self.timeout = 20000
+
+        pg.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+
     def run(self, ):
+        pg.mixer_music.load(f'./asset/{self.name}.mp3')
+        pg.mixer_music.play(-1)
+        clock = pg.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
+
+            self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pg.display.flip()
-            pass
+        pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pg.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
