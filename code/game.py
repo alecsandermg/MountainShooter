281712d2c12e5a08d3dc4e3f340af0e9ef19deac
Alecsander <alecsandermg@hotmail.com>
2025-08-27 20:45:59 -0300

    SQL implemented for score using proxy design pattern

--- code/game.py
+++ code/game.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame as pg
+
+from code.Score import Score
 from code.level import Level
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.menu import Menu
@@ -13,6 +15,7 @@ class Game:
     def run(self, ):
 
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -23,6 +26,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pg.quit()
